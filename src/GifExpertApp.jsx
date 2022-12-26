53649aeab73a6134d0eb2ef3bc7ad24895d8b39c
Mauro <m_pelegri@gmail.com>
2022-12-26 16:32:44 -0300

    06 - GifExpertApp - hasta clase 013 Fetch API

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,30 +1,24 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+  const [categories, setCategories] = useState(['One Punch']);
+
   const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
     setCategories([newCategory, ...categories]);
   };
 
   return (
     <>
-      {/* Titulo */}
       <h1>GifExpertApp</h1>
-      {/* Input */}
-      <AddCategory
-        //setCategories={setCategories}
-        onNewCategory={(event) => onAddCategory(event)}
-      />
-      {/* Listado de Gif */}
+      <AddCategory onNewCategory={(event) => onAddCategory(event)} />
 
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-        {/* <li>ABC</li> */}
-      </ol>
-      {/* Gif Item */}
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
